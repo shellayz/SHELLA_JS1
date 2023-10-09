7d78e02c4c786cfc25e3a598706ae5c10b3ac50e
shellayz <146356710+shellayz@users.noreply.github.com>
2023-10-09 11:30:58 +0700

    Delete for.js
    hapus file

--- for.js
+++
@@ -1,5 +0,0 @@
-let bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
-
-for (let i = 0; i < bulan.length; i++) {
-  console.log("Bulan ke-" + (i + 1) + " adalah bulan " + bulan[i]);
-}
